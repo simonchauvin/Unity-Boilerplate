7cae6cc8a6c2cd295db7307ca0aa90f05e586aa9
Simon Chauvin <chauvin.simon@gmail.com>
2014-09-13 16:29:17 +0200

    Basic menu and game scenes with in game menu

---
+++ 2D/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Return) || Input.GetKeyDown(KeyCode.KeypadEnter))
+		{
+			Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}

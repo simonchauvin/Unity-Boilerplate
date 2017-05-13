8da1344ec20932ab820249b453ca9b6319c084c9
Simon Chauvin <contact@simonchauvin.com>
2017-05-13 11:51:41 +0200

    Refactored screenshot capture to add configurable parameters.

---
+++ Assets/Scripts/ScreenshotCapture.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System;
+using System.IO;
+
+public class ScreenshotCapture : MonoBehaviour
+{
+    public string directoryName = "Screenshots";
+    public KeyCode captureKey = KeyCode.P;
+    public bool singleScreenshotMode = true;
+    public int resolutionWidth;
+    public int resolutionHeight;
+
+    private string subDirectoryName;
+    private Camera thisCamera;
+    private RenderTexture renderTex;
+    private Texture2D screenshotTexture;
+
+
+	void Start ()
+    {
+#if UNITY_EDITOR
+        if (directoryName.Length > 0)
+        {
+            if (!Directory.Exists(directoryName))
+            {
+                Directory.CreateDirectory(directoryName);
+            }
+            subDirectoryName = DateTime.Now.ToString("dd-MM-yyyy");
+            if (!Directory.Exists(subDirectoryName))
+            {
+                Directory.CreateDirectory(directoryName + "/" + subDirectoryName);
+            }
+        }
+
+        thisCamera = GetComponent<Camera>();
+        renderTex = new RenderTexture(resolutionWidth, resolutionHeight, 1, RenderTextureFormat.ARGBFloat);
+        screenshotTexture = new Texture2D(resolutionWidth, resolutionHeight, TextureFormat.RGBAFloat, false);
+#endif
+    }
+	
+	void Update ()
+    {
+		
+	}
+
+    private void LateUpdate()
+    {
+#if UNITY_EDITOR
+        if (directoryName.Length > 0)
+        {
+            bool capture = false;
+            if (singleScreenshotMode)
+            {
+                capture = Input.GetKeyDown(captureKey);
+            }
+            else
+            {
+                capture = Input.GetKey(captureKey);
+            }
+
+            if (capture)
+            {
+                RenderTexture.active = renderTex;
+                thisCamera.targetTexture = renderTex;
+                thisCamera.Render();
+
+                string filename = DateTime.Now.ToString("dd-MM-yyyy_HHmmssFFFF") + ".png";
+                screenshotTexture.ReadPixels(new Rect(0, 0, resolutionWidth, resolutionHeight), 0, 0);
+                screenshotTexture.Apply();
+                File.WriteAllBytes(directoryName + "/" + subDirectoryName + "/" + filename, screenshotTexture.EncodeToPNG());
+                thisCamera.targetTexture = null;
+            }
+        }
+    }
+#endif
+}
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
 Unity-Boilerplate
 =================
 
-A simple boilerplate for any Unity game. It contains an intro screen, an in game menu, a gitignore and Unity is set for Git use.
+A simple boilerplate for any Unity game.
+
+It contains:
+ 
+* A singleton game manager
+* A singleton audio manager
+* An intro screen
+* An in game menu
+* The gitignore file for Unity and Visual Studio
+* Unity settings for Git use
+* A configurable screenshot capture tool
